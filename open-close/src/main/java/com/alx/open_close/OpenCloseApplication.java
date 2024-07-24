d98e982603c252310cd88adeab08edafee741b32
AlexandreSout0 <soutoinf@gmail.com>
2024-07-24 20:48:47 -0300

    OK

--- open-close/src/main/java/com/alx/open_close/OpenCloseApplication.java
+++ open-close/src/main/java/com/alx/open_close/OpenCloseApplication.java
@@ -1,5 +1,6 @@
 package com.alx.open_close;
 
+import com.alx.open_close.vehicles.Motocycle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +10,14 @@ import java.lang.reflect.Type;
 public class OpenCloseApplication {
 
 	public static void main(String[] args) {
-		final TypeVehicle typeVehicle = TypeVehicle.CAR;
+		final TypeVehicle typeVehicle = TypeVehicle.MOTORCYCLE;
 
 		SpringApplication.run(OpenCloseApplication.class, args);
 		if(typeVehicle == TypeVehicle.CAR){
-			Vehicle vehicle = new Vehicle("Azul", "2021", 2.0, 4);
-			vehicle.car();
+			//Vehicle vehicle = new Vehicle("Azul", "2021", 2.0, 4);
+			//vehicle.car();
+		}else if (typeVehicle == TypeVehicle.MOTORCYCLE){
+			Motocycle motorcycle = new Motocycle("branca", "2022", 250);
 		}
 	}
 
--- open-close/src/main/java/com/alx/open_close/vehicles/Motocycle.java
+++ open-close/src/main/java/com/alx/open_close/vehicles/Motocycle.java
@@ -1,10 +1,11 @@
 package com.alx.open_close.vehicles;
 
 public class Motocycle extends Vehicle {
-    public Motocycle(String color, String year, double engine, int seats) {
+    public Motocycle(String color, String year, double engine) {
         this.color = color;
         this.year = year;
         this.engine = engine;
+        configureMotocycle();
     }
     public void configureMotocycle(){
         System.out.println("Criando uma moto - Ano:" +year + "Motor:" + engine );
